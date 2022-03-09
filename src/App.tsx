4c8c8c2d419d103d7fe5d4b25bb3557581cdf1fc
yusufisbilir <dev.yusufisbilir@gmail.com>
2022-03-09 11:55:15 +0300

    Add marker cluster.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import teslaData from './data/tesla-sites.json';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 function App() {
   return (
@@ -10,19 +11,21 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      {teslaData.map((location) => (
-        <Marker
-          key={location.id}
-          position={[location.gps.latitude, location.gps.longitude]}
-        >
-          <Popup>
-            <div>
-              <h2>{location.name}</h2>
-              <p>{`Status: ${location.status}`}</p>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
+      <MarkerClusterGroup>
+        {teslaData.map((location) => (
+          <Marker
+            key={location.id}
+            position={[location.gps.latitude, location.gps.longitude]}
+          >
+            <Popup>
+              <div>
+                <h2>{location.name}</h2>
+                <p>{`Status: ${location.status}`}</p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 }
